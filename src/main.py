671d47a54f82183ce89693f707061a51ea88ee34
emilyrhee <emilyrhee99@gmail.com>
2023-07-26 14:16:09 -0400

    made position and size into Vector2

--- src/main.py
+++ src/main.py
@@ -3,10 +3,8 @@ import sys
 
 class Player:
     def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
-        self.width = 50
-        self.height = 100
+        self.pos = pygame.math.Vector2((x, y))
+        self.size = pygame.math.Vector2((50, 100))
         self.speed = 2
 
     def draw(self, screen):
@@ -14,36 +12,35 @@ class Player:
             screen,
             "blue",
             pygame.Rect(
-                self.x - self.width / 2,
-                self.y - self.height / 2,
-                self.width,
-                self.height
+                self.pos.x - self.size.x / 2,
+                self.pos.y - self.size.y / 2,
+                self.size.x,
+                self.size.y
             )
         )
     
     def move_up(self):
-        self.y -= self.speed
+        self.pos.y -= self.speed
     
     def move_down(self):
-        self.y += self.speed
+        self.pos.y += self.speed
     
     def move_left(self):
-        self.x -= self.speed
+        self.pos.x -= self.speed
 
     def move_right(self):
-        self.x += self.speed
+        self.pos.x += self.speed
 
 class Image:
     def __init__(self, x: float, y: float, img_path: str):
-        self.x = x
-        self.y = y
+        self.pos = pygame.math.Vector2((x, y))
         self.img_path = img_path
 
     def draw(self, screen: pygame.Surface):
-        screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0))  # move out of Image class
         screen.blit(
             pygame.image.load(self.img_path).convert(),
-            (self.x, self.y)
+            (self.pos.x, self.pos.y)
         )
 
 def exit_on_close():
